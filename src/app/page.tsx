be0323025c8df439dc14eb1946fa64063cd687f7
Chu1503 <shivakash2003@gmail.com>
2025-07-03 23:32:02 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-// import Chu from "@/components/Chu";
+import Chu from "@/components/Chu";
 import { cubicBezier, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { ShimmerButton } from "@/components/magicui/shimmer-button";
+import { AuroraText } from "@/components/magicui/aurora-text";
 import { ExternalLink } from "lucide-react";
 
 const container = {
@@ -44,8 +45,8 @@ export default function Home() {
         </filter>
         <rect width="100%" height="100%" filter="url(#noiseFilter)" />
       </svg> */}
-      <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full px-6 md:px-16 xl:px-48">
-        <div className="w-full md:w-1/2 xl:w-2/3">
+      <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full px-6 md:px-16 xl:px-36">
+        <div className="w-full md:w-2/3 xl:w-2/3">
           <motion.div
             variants={container}
             initial="hidden"
@@ -54,7 +55,7 @@ export default function Home() {
           >
             <motion.p
               variants={slideUp}
-              className="text-2xl font-medium text-[#e8e8e8] "
+              className="text-2xl md:text-4xl xl:text-4xl font-medium text-[#e8e8e8] "
             >
               Hey
               <Image
@@ -67,16 +68,15 @@ export default function Home() {
             </motion.p>
             <motion.h1
               variants={slideUp}
-              className="text-6xl md:text-4xl xl:text-8xl font-bold 
-              text-red-500 md:text-blue-500 xl:text-green-500
+              className="text-6xl md:text-7xl xl:text-8xl font-bold 
+              text-[#e8e8e8]
               "
             >
-              {/* text-[#e8e8e8]  */}
-              I{"'"}m Shiv Akash!
+              I{"'"}m <AuroraText>Shiv Akash</AuroraText>
             </motion.h1>
             <motion.p
               variants={slideUp}
-              className="text-xl mx-auto tracking-wide font-normal text-[#bbbbbb]"
+              className="text-lg sm:text-xl mx-auto tracking-wide font-normal text-[#bbbbbb]"
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -84,7 +84,7 @@ export default function Home() {
             <motion.div variants={slideUp}>
               <Link href="/Shiv-Akash-Resume.pdf" target="_blank" download>
                 <ShimmerButton>
-                  <span className="flex items-center gap-2 text-sm">
+                  <span className="flex items-center gap-2 text-sm md:text-lg">
                     Resume
                     <ExternalLink className="h-4 w-4" />
                   </span>
@@ -93,8 +93,8 @@ export default function Home() {
             </motion.div>
           </motion.div>
         </div>
-        <div className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]">
-          {/* <Chu /> */}
+        <div className="w-full md:w-1/3 xl:w-1/3 flex items-center justify-center h-[200px] md:h-[325px] xl:h-[475px]">
+          <Chu />
         </div>
       </div>
     </main>
